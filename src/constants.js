a7a3e2d2c991ba47202e23ad888654fc19c1c830
nestormelendez <melendezinv4321@gmail.com>
2024-08-19 17:14:58 -0400

    render url https

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 const BASE_URL = "nestbook-backend.onrender.com"
 // const BASE_URL = "localhost:4000"
-export const API_URL = `http://${BASE_URL}`;
+export const API_URL = `https://${BASE_URL}`;
 export const WEBSOCKET_URL = `wss://${BASE_URL}`
+
